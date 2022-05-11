b36ac9115ef4b0e1bbcec2edd70ce7ba92fd6496
Vladimir <luxorchegone@gmail.com>
2022-05-11 21:47:11 +0300

    Создал компонент тег

---
+++ components/Tag/Tag.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { TagProps } from './Tag.props';
+import styles from './Tag.module.css';
+import cn from 'classnames';
+
+export const Tag = ({
+  size = 'm',
+  children,
+  href,
+  color = 'ghost',
+  className,
+  ...props
+}: TagProps): JSX.Element => {
+  return (
+    <div
+      className={cn(styles.tag, className, {
+        [styles.red]: color == 'red',
+        [styles.ghost]: color == 'ghost',
+        [styles.primary]: color == 'primary',
+        [styles.grey]: color == 'grey',
+        [styles.green]: color == 'green',
+        [styles.s]: size == 's',
+        [styles.m]: size == 'm',
+      })}
+      {...props}
+    >
+      {href ? <a href={href}>{children}</a> : <>{children}</>}
+    </div>
+  );
+};
