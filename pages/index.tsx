17e49e873ec338d7f872b701a20330a12d958cfa
Luxorchegone <luxorchegone@gmail.com>
2022-05-11 20:35:20 +0700

    Мелкие правки по проекту

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Htag } from '../components';
+import { Htag, Button, Ptag } from '../components';
 
 export default function Home(): JSX.Element {
   return (
     <div>
       <Htag tag="h1">Например</Htag>
+      <Button arrow={'right'} appearance='primary' className='testdff'>Это кнопка</Button>
+      <Button arrow={'right'} appearance='ghost'>Это кнопка</Button>
+      <Ptag size={'s'}>
+        Маленький
+      </Ptag>
+      <Ptag size={'m'}>
+        Средний
+      </Ptag>
+      <Ptag size={'l'}>
+        Большой
+      </Ptag>
     </div>
   );
 }
