34f41b8e565727d2ed509c0c937450253ad7405e
Luxorchegone <luxorchegone@gmail.com>
2022-05-11 20:34:02 +0700

    Создал компонент параграф

---
+++ components/Ptag/Ptag.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { PtagProps } from "./Ptag.props";
+import styles from './Ptag.module.css';
+import cn from 'classnames'ж
+
+export const Ptag = ({ size, children, className, ...props}: PtagProps): JSX.Element => {
+  return (<p className={cn(styles.p, className, {
+    [styles.s]: size == 's',
+    [styles.m]: size == 'm',
+    [styles.l]: size == 'l',
+  })} {...props}>
+    {children}
+  </p>);
+};
