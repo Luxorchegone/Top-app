42b4f7f34fc4a1605a921f4d6b2a783d0d1fe215
Vladimir <luxorchegone@gmail.com>
2022-05-22 15:18:08 +0300

    Вынес разделы меню в отдельный файл/папку

---
+++ helpers/helpers.tsx
@@ -0,0 +1,33 @@
+import { TopLevelCategory } from '../interfaces/page.interface';
+import CoursesIcon from './icons/courses.svg';
+import BooksIcon from './icons/books.svg';
+import ServicesIcon from './icons/services.svg';
+import ProductsIcon from './icons/products.svg';
+import { FirstLevelMenuItem } from '../interfaces/menu.interface';
+
+export const firstLevelMenu: FirstLevelMenuItem[] = [
+  {
+    route: "courses",
+    name: "Курсы",
+    icon: <CoursesIcon />,
+    id: TopLevelCategory.Courses,
+  },
+  {
+    route: "books",
+    name: "Книги",
+    icon: <BooksIcon />,
+    id: TopLevelCategory.Books,
+  },
+  {
+    route: "services",
+    name: "Сервисы",
+    icon: <ServicesIcon />,
+    id: TopLevelCategory.Services,
+  },
+  {
+    route: "products",
+    name: "Продукты",
+    icon: <ProductsIcon />,
+    id: TopLevelCategory.Products,
+  },
+];
