4fc6cee41a271e91571f8bd8c589fa295b42520e
Luxorchegone <luxorchegone@gmail.com>
2022-05-25 21:05:39 +0700

    Создал компонент для отображения преимуществ продукта

---
+++ components/Advantages/Advantages.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { AdvantagesProps } from './Advantages.props';
+import styles from './Advantages.module.css';
+import cn from 'classnames';
+import CheckIcon from './check.svg';
+
+export const Advantages = ({ advantages }: AdvantagesProps): JSX.Element => {
+  return (
+    <>
+      {advantages.map((item) => (
+        <div key={item._id} className={styles.advantage}>
+          <CheckIcon/>
+          <div className={styles.title}>{item.title}</div>
+          <hr className={styles.verticalLine}/>
+          <div className={styles.description}>{item.description}</div>
+        </div>
+      ))}
+    </>
+  );
+};
