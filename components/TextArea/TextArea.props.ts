05c6443710ed529cf9839263d101e000d67d02bc
Vladimir <luxorchegone@gmail.com>
2022-05-31 20:46:21 +0300

    Создал компонент - кастомный TextArea

---
+++ components/TextArea/TextArea.props.ts
@@ -0,0 +1,5 @@
+import { DetailedHTMLProps, InputHTMLAttributes, TextareaHTMLAttributes,  } from 'react';
+
+export interface TextAreaProps extends DetailedHTMLProps<TextareaHTMLAttributes<HTMLTextAreaElement>, HTMLTextAreaElement> {
+
+}
