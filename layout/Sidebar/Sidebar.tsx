b3424045bb4a9a12ba61366619344244ffcf2e1f
Vladimir <luxorchegone@gmail.com>
2022-05-31 21:30:39 +0300

    Добавил компонент поиска в сайдбар

--- layout/Sidebar/Sidebar.tsx
+++ layout/Sidebar/Sidebar.tsx
@@ -4,11 +4,12 @@ import styles from './Sidebar.module.css';
 import cn from 'classnames';
 import { Menu } from '../Menu/Menu';
 import Logo from '../logo.svg';
+import { Search } from '../../components';
 
 export const Sidebar = ({className, ...props }: SidebarProps): JSX.Element => {
   return  <div className={cn(className, styles.sidebar)} {...props} >
 		<Logo className={styles.logo}/>
-		<div>Поиск</div>
-		<Menu/>
+		<Search />
+		<Menu />
 	</div>;
 };
