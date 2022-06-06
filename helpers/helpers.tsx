44766b8de8091b3d5759ccb5d13f57c831239b68
Luxorchegone <luxorchegone@gmail.com>
2022-06-06 13:33:48 +0300

    Написал функцию для правильного склонения слов

--- helpers/helpers.tsx
+++ helpers/helpers.tsx
@@ -7,29 +7,41 @@ import { FirstLevelMenuItem } from '../interfaces/menu.interface';
 
 export const firstLevelMenu: FirstLevelMenuItem[] = [
   {
-    route: "courses",
-    name: "Курсы",
+    route: 'courses',
+    name: 'Курсы',
     icon: <CoursesIcon />,
     id: TopLevelCategory.Courses,
   },
   {
-    route: "books",
-    name: "Книги",
+    route: 'books',
+    name: 'Книги',
     icon: <BooksIcon />,
     id: TopLevelCategory.Books,
   },
   {
-    route: "services",
-    name: "Сервисы",
+    route: 'services',
+    name: 'Сервисы',
     icon: <ServicesIcon />,
     id: TopLevelCategory.Services,
   },
   {
-    route: "products",
-    name: "Продукты",
+    route: 'products',
+    name: 'Продукты',
     icon: <ProductsIcon />,
     id: TopLevelCategory.Products,
   },
 ];
 
-export const priceRu = (price: number): string => price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ' ').concat(' ₽');
+//Визуально отделяем тысячи от числа и добавляем знак рубля
+export const priceRu = (price: number): string =>
+  price
+    .toString()
+    .replace(/\B(?=(\d{3})+(?!\d))/g, ' ')
+    .concat(' ₽');
+
+//склоняем слова правильно
+export const declOfNum = (number: number, titles: [string, string, string]): string => {
+  //id елемента - кол-во отзывов, а сам элемент - номер варианта склонения слова
+  const cases = [2, 0, 1, 1, 1, 2];
+  return titles[number % 100 > 4 && number % 100 < 20 ? 2 : cases[number % 10 < 5 ? number % 10 : 5]];
+};
