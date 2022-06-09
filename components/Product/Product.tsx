84b76ef47e19465a40edde843e4715712b3638f6
Luxorchegone <luxorchegone@gmail.com>
2022-06-09 17:22:45 +0300

    Добавил адаптивности верстке компонента продукта

--- components/Product/Product.tsx
+++ components/Product/Product.tsx
@@ -72,7 +72,7 @@ export const Product = ({ product, className, ...props }: ProductProps): JSX.Ele
           </div>
         )}
       </div>
-      <Divider className={styles.hr} />
+      <Divider className={cn(styles.hr, styles.hr2)} />
       <div className={styles.actions}>
         <Button appearance="primary">Узнать подробнее</Button>
         <Button className={styles.reviewButton} appearance="ghost" arrow="right">
