acf7962e1803d6662cc474fe0077ed11bbaa87df
Luxorchegone <luxorchegone@gmail.com>
2022-06-24 15:34:52 +0300

    Добавил валидацию формы с помощью react-hook-form

--- components/ReviewForm/ReviewForm.tsx
+++ components/ReviewForm/ReviewForm.tsx
@@ -7,30 +7,53 @@ import { Rating } from '../Rating/Rating';
 import { TextArea } from '../TextArea/TextArea';
 import { Button } from '../Button/Button';
 import CloseIcon from './close.svg';
+import { Controller, useForm } from 'react-hook-form';
+import { IReviewForm } from './ReviewForm.interface';
 
 export const ReviewForm = ({ productId, className, ...props }: ReviewFormProps): JSX.Element => {
+  const { handleSubmit, register, control, formState: { errors } } = useForm<IReviewForm>();
+
+  const onSubmit = (data: IReviewForm) => {
+    console.log(data);
+  };
+
   return (
-    <>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div className={cn(styles.reviewForm, className)} {...props}>
-        <Input placeholder="Имя" />
-        <Input placeholder="Заголовок отзыва" className={styles.titleInput} />
+        <Input 
+          {...register('name', { required: {value: true, message: 'Заполните имя'} })} 
+          placeholder="Имя" 
+          error={errors.name}
+        />
+        <Input 
+          {...register('title', { required: {value: true, message: 'Заполните заголовок'} })} 
+          placeholder="Заголовок отзыва" 
+          className={styles.titleInput} 
+          error={errors.title}
+        />
         <div className={styles.rating}>
           <span>Оценка:</span>
-          <Rating rating={0} />
+          <Controller
+            control={control}
+            name="rating"
+            render={({ field }) => <Rating isEditable ref={field.ref} rating={field.value} setRating={field.onChange} />}
+          />
         </div>
-        <TextArea placeholder="Текст отзыва" className={styles.description} />
+        <TextArea 
+          {...register('description', { required: {value: true, message: 'Заполните отзыв'} })} 
+          placeholder="Текст отзыва" 
+          className={styles.description} 
+          error={errors.description}/>
         <div className={styles.submit}>
           <Button appearance="primary">Отправить</Button>
           <span className={styles.info}>* Перед публикацией отзыв пройдет предварительную модерацию и проверку</span>
         </div>
       </div>
-			<div className={styles.success}>
-				<div className={styles.successTitle}>Ваш отзыв отправлен</div>
-				<div>
-					Спасибо. ваш отзыв будет опубликован после проверки!
-				</div>
-				<CloseIcon className={styles.closeIcon} />
-			</div>
-    </>
+      <div className={styles.success}>
+        <div className={styles.successTitle}>Ваш отзыв отправлен</div>
+        <div>Спасибо. ваш отзыв будет опубликован после проверки!</div>
+        <CloseIcon className={styles.closeIcon} />
+      </div>
+    </form>
   );
 };
