03dce71da453d8e55f9f9d8441c75a843d0fc9da
Vladimir <luxorchegone@gmail.com>
2022-06-26 21:50:04 +0300

    Рефакторинг API

--- pages/[type]/[alias].tsx
+++ pages/[type]/[alias].tsx
@@ -3,20 +3,15 @@ import React from 'react';
 import { withLayout } from '../../layout/Layout';
 import axios from 'axios';
 import { MenuItem } from '../../interfaces/menu.interface';
-import {
-  TopLevelCategory,
-  TopPageModel,
-} from '../../interfaces/page.interface';
+import { TopLevelCategory, TopPageModel } from '../../interfaces/page.interface';
 import { ParsedUrlQuery } from 'querystring';
 import { ProductModel } from '../../interfaces/product.interface';
 import { firstLevelMenu } from '../../helpers/helpers';
 import { TopPageComponent } from '../../page-components';
+import { API } from '../../helpers/api';
 
-function TopPage({firstCategory, menu, page, products }: CourseProps): JSX.Element {
-  return <TopPageComponent 
-		firstCategory={firstCategory}
-		page={page}
-		products={products}/>;
+function TopPage({ firstCategory, menu, page, products }: CourseProps): JSX.Element {
+  return <TopPageComponent firstCategory={firstCategory} page={page} products={products} />;
 }
 
 export default withLayout(TopPage);
@@ -26,14 +21,12 @@ export const getStaticPaths: GetStaticPaths = async () => {
 
   for (const m of firstLevelMenu) {
     //получаем данные по всем категориям
-    const { data: menu } = await axios.post<MenuItem[]>(
-      process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
-      { firstCategory: m.id }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, {
+      firstCategory: m.id,
+    });
     paths = paths.concat(
       //убираем вложенность => получаем массив статических путей
-      menu.flatMap((item) =>item.pages.map((page) => `/${m.route}/${page.alias}`)
-      )
+      menu.flatMap((item) => item.pages.map((page) => `/${m.route}/${page.alias}`))
     );
   }
   return {
@@ -62,27 +55,19 @@ export const getStaticProps: GetStaticProps<CourseProps> = async ({
 
   try {
     //получаем список направлений курсов
-    const { data: menu } = await axios.post<MenuItem[]>(
-      process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
-      { firstCategory: firstCategoryItem.id }
-    );
+    const { data: menu } = await axios.post<MenuItem[]>(API.topPage.find, { firstCategory: firstCategoryItem.id });
     if (menu.length == 0) {
       return {
         notFound: true,
       };
     }
     //получаем описание данного направления подготовки
-    const { data: page } = await axios.get<TopPageModel>(
-      process.env.NEXT_PUBLIC_DOMAIN + `/api/top-page/byAlias/${params.alias}`
-    );
+    const { data: page } = await axios.get<TopPageModel>(API.topPage.byAlias + params.alias);
     //получаем список доступных материалов для подготовки по данному направлению
-    const { data: products } = await axios.post<ProductModel[]>(
-      process.env.NEXT_PUBLIC_DOMAIN + '/api/product/find',
-      {
-        category: page.category,
-        limit: 10,
-      }
-    );
+    const { data: products } = await axios.post<ProductModel[]>(API.product.find, {
+      category: page.category,
+      limit: 10,
+    });
 
     return {
       props: {
