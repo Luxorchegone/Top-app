ac628d39a542fe32bb094b3d7130922d870d0626
Luxorchegone <luxorchegone@gmail.com>
2022-07-04 17:11:38 +0300

    Добавил интерфейс для ответа сервера, после отправки формы комеентария

--- components/ReviewForm/ReviewForm.interface.ts
+++ components/ReviewForm/ReviewForm.interface.ts
@@ -1,8 +1,10 @@
-import { ReviewsModel } from '../../interfaces/product.interface';
-
 export interface IReviewForm {
   name: string;
   title: string;
   description: string;
   rating: number;
+}
+
+export interface IReviewSentResponse {
+  message: string;
 }
