dd338addb396cedff31731139429db702e0e1bd6
Luxorchegone <luxorchegone@gmail.com>
2022-07-11 17:12:17 +0300

    Добавил кнопку переключения фокуса на контент в обход перебора пунктов меню

--- layout/Layout.tsx
+++ layout/Layout.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, KeyboardEvent, useRef } from 'react';
 import { LayoutProps } from './Layout.props';
 import { Sidebar } from './Sidebar/Sidebar';
 import { Footer } from './Footer/Footer';
@@ -6,22 +6,46 @@ import { Header } from './Header/Header';
 import styles from './Layout.module.css';
 import { AppContextProvider, IAppContext } from '../context/app.context';
 import { UpButton } from '../components';
+import cn from 'classnames';
 
 const Layout = ({ children }: LayoutProps): JSX.Element => {
+  const [isSkipLinkDisplayed, setIsSkipLinkdisplayed] = useState<boolean>(false);
+  const bodyRef = useRef<HTMLDivElement>(null);
+
+  const skipContentAction = (key: KeyboardEvent) => {
+    if (key.code == 'Space' || key.code == 'Enter') {
+      key.preventDefault();
+      bodyRef.current?.focus();
+      setIsSkipLinkdisplayed(false);
+    } else {
+      setIsSkipLinkdisplayed(false);
+    }
+  };
+
   return (
     <div className={styles.wrapper}>
+      <a
+        onFocus={() => setIsSkipLinkdisplayed(true)}
+        onKeyDown={skipContentAction}
+        tabIndex={1}
+        className={cn(styles.skipLink, {
+          [styles.displayed]: isSkipLinkDisplayed,
+        })}
+      >
+        Сразу к содержанию
+      </a>
       <Header className={styles.header} />
       <Sidebar className={styles.sidebar} />
-      <div className={styles.body}>{children}</div>
+      <div className={styles.body} ref={bodyRef} tabIndex={0}>
+        {children}
+        </div>
       <Footer className={styles.footer} />
-			<UpButton />
+      <UpButton />
     </div>
   );
 };
 
-export const withLayout = <T extends Record<string, unknown> & IAppContext>(
-  Component: React.FC<T>
-) => {
+export const withLayout = <T extends Record<string, unknown> & IAppContext>(Component: React.FC<T>) => {
   return function withLayoutComponent(props: T): JSX.Element {
     return (
       <AppContextProvider menu={props.menu} firstCategory={props.firstCategory}>
