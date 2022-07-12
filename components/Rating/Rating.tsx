393e1a5dc6429267b26203e560f604dc90ac73b3
Luxorchegone <luxorchegone@gmail.com>
2022-07-12 14:24:23 +0300

    Сделал компонент рейтинга доступным с клавиатуры. Добавлено выставление рейтинга с помощью клавиш. Добавлен автоматический tabIndex, для правильного управления фокусом

--- components/Rating/Rating.tsx
+++ components/Rating/Rating.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef } from 'react';
+import React, { useEffect, useState, KeyboardEvent, forwardRef, ForwardedRef, useRef } from 'react';
 import { RatingProps } from './Rating.props';
 import styles from './Rating.module.css';
 import cn from 'classnames';
@@ -7,14 +7,15 @@ import classNames from 'classnames';
 
 export const Rating = forwardRef(
   (
-    { isEditable = false, setRating, rating, error, className, ...props }: RatingProps,
+    { isEditable = false, setRating, rating, error, className, tabIndex, ...props }: RatingProps,
     ref: ForwardedRef<HTMLDivElement>
   ): JSX.Element => {
     const [ratingArray, setRatingArray] = useState<JSX.Element[]>(new Array(5).fill(<></>));
+    const ratingArrayRef = useRef<(HTMLSpanElement | null)[]>([]);
 
     useEffect(() => {
       ratingConstructor(rating);
-    }, [rating]);
+    }, [rating, tabIndex]);
 
     const ratingConstructor = (currentRating: number) => {
       const updatedArray = ratingArray.map((item: JSX.Element, i: number) => {
@@ -27,15 +28,28 @@ export const Rating = forwardRef(
             onMouseEnter={() => changeDisplay(i + 1)}
             onMouseLeave={() => changeDisplay(rating)}
             onClick={() => onClick(i + 1)}
+            tabIndex={computeFocus(rating, i)}
+            onKeyDown={handleKey}
+            ref={(r)=>ratingArrayRef.current.push(r)}
           >
-            <StarIcon
-              tabIndex={isEditable ? 0 : -1}
-              onKeyDown={(e: KeyboardEvent<SVGElement>) => isEditable && handleSpace(i + 1, e)}
-            />
+            <StarIcon />
           </span>
         );
       });
       setRatingArray(updatedArray);
+    };
+
+    const computeFocus = (r: number, i: number): number => {
+      if (!isEditable) {
+        return -1;
+      }
+      if (!rating && i == 0) {
+        return tabIndex ?? 0;
+      }
+      if (r == i + 1) {
+        return tabIndex ?? 0;
+      }
+      return -1;
     };
 
     const changeDisplay = (i: number) => {
@@ -52,11 +66,25 @@ export const Rating = forwardRef(
       setRating(i);
     };
 
-    const handleSpace = (i: number, e: KeyboardEvent<SVGElement>) => {
-      if (e.code != 'Space' || !setRating) {
+    const handleKey = (e: KeyboardEvent) => {
+      if (!isEditable || !setRating) {
         return;
       }
-      setRating(i);
+      if (e.code == 'ArrowRight' || e.code == 'ArrowUp') {
+        if (!rating) {
+          setRating(1);
+        } else {
+          e.preventDefault();
+          setRating(rating < 5 ? rating + 1 : 5);
+        }
+        ratingArrayRef.current[rating]?.focus();
+      }
+      if (e.code == 'ArrowLeft' || e.code == 'ArrowDown') {
+        e.preventDefault();
+        setRating(rating > 1 ? rating - 1 : 1);
+        ratingArrayRef.current[rating - 2]?.focus();
+      }
+      
     };
 
     return (
