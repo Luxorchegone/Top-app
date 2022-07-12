42aa5aaca699a9fe711b1d88f1d3067856c3906e
Vladimir <luxorchegone@gmail.com>
2022-07-12 21:28:28 +0300

    Добавил навигацию с помощью клавиатуры

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -1,20 +1,21 @@
-import React from 'react';
+import React, { KeyboardEvent } from 'react';
 import { SortEnum, SortProps } from './Sort.props';
 import SortIcon from './sort.svg';
 import styles from './Sort.module.css';
 import cn from 'classnames';
 
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
+
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <span className={cn({ [styles.active]: sort == SortEnum.Rating })} onClick={() => setSort(SortEnum.Rating)}>
+      <button className={cn({ [styles.active]: sort == SortEnum.Rating })} onClick={() => setSort(SortEnum.Rating)}>
         <SortIcon className={styles.sortIcon} />
         По рейтингу
-      </span>
-      <span className={cn({ [styles.active]: sort == SortEnum.Price })} onClick={() => setSort(SortEnum.Price)}>
+      </button>
+      <button className={cn({ [styles.active]: sort == SortEnum.Price })} onClick={() => setSort(SortEnum.Price)}>
         <SortIcon className={styles.sortIcon} />
         По цене
-      </span>
+      </button>
     </div>
   );
 };
