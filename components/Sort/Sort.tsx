cc6692f99d61bf64406d6cbd079ae740c5bf46cb
Luxorchegone <luxorchegone@gmail.com>
2022-07-13 12:42:44 +0300

    Добавил доступности компоненту сортировки

--- components/Sort/Sort.tsx
+++ components/Sort/Sort.tsx
@@ -5,14 +5,28 @@ import styles from './Sort.module.css';
 import cn from 'classnames';
 
 export const Sort = ({ sort, setSort, className, ...props }: SortProps): JSX.Element => {
-
   return (
     <div className={cn(styles.sort, className)} {...props}>
-      <button className={cn({ [styles.active]: sort == SortEnum.Rating })} onClick={() => setSort(SortEnum.Rating)}>
+      <div className={styles.sortName} id="sort">
+        Сортировка
+      </div>
+      <button
+        id="rating"
+        className={cn({ [styles.active]: sort == SortEnum.Rating })}
+        onClick={() => setSort(SortEnum.Rating)}
+        aria-selected={sort == SortEnum.Rating}
+        aria-labelledby="sort rating"
+      >
         <SortIcon className={styles.sortIcon} />
         По рейтингу
       </button>
-      <button className={cn({ [styles.active]: sort == SortEnum.Price })} onClick={() => setSort(SortEnum.Price)}>
+      <button
+        id="price"
+        className={cn({ [styles.active]: sort == SortEnum.Price })}
+        onClick={() => setSort(SortEnum.Price)}
+        aria-selected={sort == SortEnum.Price}
+        aria-labelledby="sort price"
+      >
         <SortIcon className={styles.sortIcon} />
         По цене
       </button>
