33c530a26b0986b0303fdc6527d62a26020e4a8d
Luxorchegone <luxorchegone@gmail.com>
2022-07-15 12:27:37 +0300

    Добавил доступность кнопке вверх

--- components/UpButton/UpButton.tsx
+++ components/UpButton/UpButton.tsx
@@ -2,30 +2,27 @@ import React, { useEffect } from 'react';
 import styles from './UpButton.module.css';
 
 import { useScrollY } from '../../hooks/useScrollY';
-import { useAnimation, motion} from 'framer-motion';
+import { useAnimation, motion } from 'framer-motion';
 import { ButtonIcon } from '../ButtonIcon/ButtonIcon';
 
 export const UpButton = (): JSX.Element => {
-	const controls = useAnimation();
-	const y = useScrollY();
+  const controls = useAnimation();
+  const y = useScrollY();
 
-	useEffect(()=> {
-		controls.start({ opacity: y / document.body.scrollHeight });
-	}, [y, controls])
+  useEffect(() => {
+    controls.start({ opacity: y / document.body.scrollHeight });
+  }, [y, controls]);
 
-	const scrollToTop = () => {
-		window.scrollTo({
-			top: 0,
-			behavior: 'smooth',
-		});
-	};
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
 
-	return (
-		<motion.div 
-			className={styles.upButton} 
-			animate={controls}
-			initial={{opacity: 0}}>
-			<ButtonIcon appearance='primary' icon='up' onClick={scrollToTop}/>
-		</motion.div>
-	);
+  return (
+    <motion.div className={styles.upButton} animate={controls} initial={{ opacity: 0 }}>
+      <ButtonIcon appearance="primary" icon="up" aria-label="Наверх" onClick={scrollToTop} />
+    </motion.div>
+  );
 };
